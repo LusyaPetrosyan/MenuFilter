c48cbb1d1dd71827c5b3168a6723d3e7fe6da85c
LusyaPetrosyan <96180298+LusyaPetrosyan@users.noreply.github.com>
2022-02-20 00:03:54 +0400

    Menu

---
+++ .history/menuFilter/app_20220217170748.js
@@ -0,0 +1,71 @@
+import { menu } from "./data.js";
+
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+  diplayMenuItems(menu);
+  displayMenuButtons();
+});
+
+function diplayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+
+    return `<article class="menu-item">
+            <img src=${item.img} alt=${item.title} class="photo" />
+            <div class="item-info">
+              <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">$${item.price}</h4>
+              </header>
+              <p class="item-text">
+                ${item.desc}
+              </p>
+            </div>
+          </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  // console.log(displayMenu);
+
+  sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons() {
+  //add display buttons here
+ 
+  }
+console.log(displayMenuButtons());
+
+const buttons = document.querySelectorAll(".filter-btn");
+buttons.forEach(filterBtn => {
+  filterBtn.addEventListener("click", () =>{
+    const value = filterBtn.getAttribute("data-id");
+    if (value === "all"){
+      return diplayMenuItems(menu);
+    }
+    const filterArr = menu.filter(item => item.category === value);
+    
+    let card = filterArr.map(function (item){
+      return `<article class="menu-item">
+      <img src=${item.img} alt=${item.title} class="photo" />
+      <div class="item-info">
+        <header>
+          <h4>${item.title}</h4>
+          <h4 class="price">$${item.price}</h4>
+        </header>
+        <p class="item-text">
+          ${item.desc}
+        </p>
+      </div>
+    </article>`;
+    }).join("");
+    sectionCenter.innerHTML = card;
+  })
+});
+const text = "";
+const search = document.querySelector("input");
+search.
+
+
